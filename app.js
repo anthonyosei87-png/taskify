6d6b648c1a55e3ddd1e217890e64aa95ece8ef0a
anthonyosei87-png <anthonyosei87@gmail.com>
2026-06-14 13:42:45 -0400

    app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:5000/api/tasks';
+const API_URL = 'https://taskify-backend-c1nm.onrender.com/api/tasks';
 
 const taskForm = document.getElementById('task-form');
 const taskTitleInput = document.getElementById('task-title');
